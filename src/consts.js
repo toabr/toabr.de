5a8ad2d60bd5ffad9a348a84bdbc824521c099f7
toabr <25079664+toabr@users.noreply.github.com>
2024-03-07 01:34:53 +0100

    add component BaseHead
    change metas
    add consts.js

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'toabr.de';
+export const SITE_DESCRIPTION = 'My digital playground, where pixels meet passion and every line of code tells a story.';
